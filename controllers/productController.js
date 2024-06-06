deea2cc6a9fab31fc6e39161241ed1a9f3e98982
harshavitorawala <141951302+harshavitorawala@users.noreply.github.com>
2024-06-06 10:26:40 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,18 +4,7 @@ import orderModel from "../models/orderModel.js";
 
 import fs from "fs";
 import slugify from "slugify";
-// import braintree from "braintree";
-// import dotenv from "dotenv";
 
-// dotenv.config();
-
-// //payment gateway
-// var gateway = new braintree.BraintreeGateway({
-//     environment: braintree.Environment.Sandbox,
-//     merchantId: process.env.BRAINTREE_MERCHANT_ID,
-//     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-//     privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-// });
 
 export const createProductController = async (req, res) => {
     try {
@@ -326,75 +315,6 @@ export const productCategoryController = async (req, res) => {
     }
 };
 
-// //payment gateway api
-// //token
-// export const braintreeTokenController = async (req, res) => {
-//     try {
-//         gateway.clientToken.generate({}, function (err, response) {
-//             if (err) {
-//                 res.status(500).send(err);
-//             } else {
-//                 res.send(response);
-//             }
-//         });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-
-// //payment
-// export const brainTreePaymentController = async (req, res) => {
-//     try {
-//         const { nonce, cart } = req.body;
-//         let total = 0;
-//         cart.map((i) => {
-//             total += i.price;
-//         });
-//         let newTransaction = gateway.transaction.sale(
-//             {
-//                 amount: total,
-//                 paymentMethodNonce: nonce,
-//                 options: {
-//                     submitForSettlement: true,
-//                 },
-//             },
-//             function (error, result) {
-//                 if (result) {
-//                     const order = new orderModel({
-//                         products: cart,
-//                         payment: result,
-//                         buyer: req.user._id,
-//                     }).save();
-//                     res.json({ ok: true });
-//                 } else {
-//                     res.status(500).send(error);
-//                 }
-//             }
-//         );
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-
-// Remove imports related to Braintree and dotenv
-
-// payment gateway api
-// Remove braintree imports and related functions
-
-// Token generation controller
-// export const TokenController = async (req, res) => {
-//     try {
-//         // Implement your own token generation logic if needed
-//         // You can use any payment gateway or method you prefer
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({
-//             success: false,
-//             message: "Error generating token",
-//             error,
-//         });
-//     }
-// };
 
 // Payment controller
 export const PaymentController = async (req, res) => {
